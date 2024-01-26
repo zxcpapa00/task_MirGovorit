7977496fe94cb177b635b0eb3b8be3e4fa3983ee
Evgeniy <krasnoperoveg@mail.ru>
2024-01-26 13:21:30 +0200

    add admin models

--- book/admin.py
+++ book/admin.py
@@ -1,2 +1,17 @@
 from django.contrib import admin
+from book.models import Product, RecipeProduct, Recipe
 
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RecipeProduct)
+class RecipeProductAdmin(admin.ModelAdmin):
+    pass
