ee41d6441a8bb525dddd2d839bbe8fa301321d8d
Evgeniy <krasnoperoveg@mail.ru>
2024-01-26 13:31:11 +0200

    rework count_uses in product model

--- book/models.py
+++ book/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=256)
-    count_uses = models.SmallIntegerField()
+    count_uses = models.SmallIntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
         return self.name
