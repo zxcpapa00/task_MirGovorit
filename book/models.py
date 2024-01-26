8e5e8994aa04ac0f3ef7b54a753ece1d2aa81c94
Evgeniy <krasnoperoveg@mail.ru>
2024-01-26 13:08:22 +0200

    add models product and recipe

--- book/models.py
+++ book/models.py
@@ -1,1 +1,17 @@
 from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256)
+    count_uses = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=512)
+    products = models.ManyToManyField(to=Product, related_name='where_uses')
+
+    def __str__(self):
+        return self.name
